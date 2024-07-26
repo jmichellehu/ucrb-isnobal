306c24c3e40a7f51bc07d5d1d22ca5f49f4b6b38
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2024-07-26 13:38:34 -0600

    scripts - update docstrings, add locate_snotel_in_poly func

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,23 +1,41 @@
 # Helper functions for working with isnobal outputs, ASO data products, and SNOTEL files
 # TODO - need to organize
 
+import os
+import sys
 from pathlib import PurePath
-import pandas as pd
-import xarray as xr
+import pathlib as p
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import xarray as xr
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.ops import unary_union
+
+import pyproj
+
+sys.path.append('/uufs/chpc.utah.edu/common/home/u6058223/git_dirs/env/')
+import helpers as h
 
 def get_varnc(basin=None, indir=None, indate=None, templatefn=None, returnvar=None):
-    '''Returns specified output files based on template file or basin and date.
-    If no specified variable, returns all files
-    basin = erw_newbasin_isnobal/
-    indir = erw_newbasin_isnobal/wy2018/erw_newbasin/
-    indate = 20180109
-    TODO
-    maybe remove basin
-    add returnvar handling for an input list
-
     '''
-    import pathlib as p
+    Obtains specified output files based on template file or basin and date.
+    
+    Parameters:
+        basin (str): The name of the basin containing iSnobal runs (e.g., erw_newbasin_isnobal/). Default is None.
+        indir (str): The input directory containing daily runs (e.g., erw_newbasin_isnobal/wy2018/erw_newbasin/). Default is None.
+        indate (str): The input date in YYYYMMDD. Default is None.
+        templatefn (str): The template file name. Default is None.
+        returnvar (str): The variable to return. Default is None and returns all files.
+        
+    Returns:
+        list: A list of output files based on the specified parameters.
+
+    TODO:maybe remove basin and add returnvar handling for an input list
+    '''
         
     # first check if you are looking for specific date based on template fn    
     if templatefn is not None:
@@ -33,13 +51,33 @@ def get_varnc(basin=None, indir=None, indate=None, templatefn=None, returnvar=No
         return h.fn_list(datedir, f'{returnvar}.nc')
     
 def extract_ravel_varvals(ds, varname='thickness'):
-    '''Extract variable values from input DataArray, default snow depth (thickness)'''
+    '''Extract variable values from input DataArray.
+
+    Parameters:
+    ds (xarray.DataArray): Input DataArray containing the variable values.
+    varname (str, optional): Name of the variable to extract. Defaults to 'thickness'.
+
+    Returns:
+    numpy.ndarray: Flattened array of variable values.
+    '''
     return np.ravel(ds[varname].values)
 
 def extract_boxplot_vals(rav_arr):
-    '''Extract outlier thresholds, 25th p, 50th p, and 75th p
-    Returns: lowwhisk, p25, p50, p75, highwhisk
-    TODO: consider using robust thresholds for this
+    '''
+    Extracts outlier thresholds, 25th percentile, 50th percentile, and 75th percentile from the given array.
+
+    Parameters:
+    rav_arr (numpy.ndarray): The input array from which the statistics will be extracted.
+
+    Returns:
+    tuple: A tuple containing the following values in order: 
+        - lowwhisk (float): The lower whisker threshold for outliers.
+        - p25 (float): The 25th percentile value.
+        - p50 (float): The 50th percentile value (median).
+        - p75 (float): The 75th percentile value.
+        - highwhisk (float): The upper whisker threshold for outliers.
+
+    TODO: consider using robust thresholds for this (16–84p)
     '''
     
     p25 = np.nanpercentile(rav_arr, 25) # first quartile
@@ -54,7 +92,16 @@ def extract_boxplot_vals(rav_arr):
     return lowwhisk, p25, p50, p75, highwhisk
 
 def fn2boxval(fn, varname='thickness'):
-    '''from snow.nc filename to boxplot values of specified variable value'''
+    '''
+    Convert input filename (e.g., snow.nc) to boxplot values of specified variable value.
+
+    Parameters:
+        fn (str): The filename of the snow.nc file.
+        varname (str, optional): The name of the variable to extract values from. Defaults to 'thickness'.
+
+    Returns:
+        list: The boxplot values of the specified variable value.
+    '''
     ds = xr.open_dataset(fn)
     raveled_vals = extract_ravel_varvals(ds, varname=varname)
     boxvals = extract_boxplot_vals(raveled_vals)
@@ -63,9 +110,18 @@ def fn2boxval(fn, varname='thickness'):
 
 
 def extract_dt(fn, inputvar="_snowdepth"):
-    """Extract date from ASO snow depth or swe filename and store in corresponding DataSet
-    Change inputvar to '_swe' if using SWE"""
-
+    '''
+    Extracts date from filename (ASO snow depth or swe) and stores it in the corresponding DataSet.
+    
+    Args:
+        fn (str): The filename from which to extract the date.
+        inputvar (str, optional): The input variable to use for extraction. 
+                                    Defaults to "_snowdepth". Change inputvar to '_swe' if using SWE files.
+    
+    Returns:
+        pd.DatetimeIndex: A pandas DatetimeIndex object representing the extracted date.
+    '''
+    
     # get date from filename and store in dataset as a single dimension?
     dt_file = PurePath(fn).name.split(inputvar)[0].split("_")[-1]
     year = int(dt_file[:4])
@@ -87,17 +143,39 @@ def extract_dt(fn, inputvar="_snowdepth"):
     return dt
 
 def assign_dt(ds, dt):
-    """Assign date value to a new time dimension for input dataset"""
+    '''
+    Assigns a datetime value to a new time dimension for the input dataset.
+
+    Parameters:
+        ds (xarray.Dataset): The input dataset.
+        dt (datetime): The date value to assign.
+
+    Returns:
+        xarray.Dataset: The dataset with the new time dimension assigned.
+    '''
     ds = ds.expand_dims(time=dt)
     return ds
 
 def calc_sdd(snow_property, verbose=False, snow_name=None):
-    '''Calculate snow disappearance date from a pandas series of snow depth or SWE
-    Expects measurement units of meters
-    Modified from From https://stackoverflow.com/questions/22000882/find-last-non-zero-elements-index-in-pandas-series
-    TODO: careful, this is not robust to late season blips, see Hoosier Pass (531) and Grizzly Peak (505) for WY 2022
     '''
+    Calculate snow disappearance date from a pandas series of snow depth or SWE.
     
+    Args:
+        snow_property (pandas.Series): A pandas series of snow depth or SWE (measurement units in meters).
+        verbose (bool, optional): If True, print additional information. Defaults to False.
+        snow_name (str, optional): Name of the snow property. Defaults to None.
+    
+    Returns:
+        tuple: A tuple containing the snow disappearance date and the first derivative of the snow property.
+    
+    Notes:
+        - This function calculates the first derivative of the snow property.
+        - It determines the last date at which the derivative is nonzero, which represents the snow disappearance date.
+        - This method is not robust to late season blips. See Hoosier Pass (531) and Grizzly Peak (505) for WY 2022 example.
+    
+    Modified from https://stackoverflow.com/questions/22000882/find-last-non-zero-elements-index-in-pandas-series
+    '''
+
     # Calculate first derivative of snow property
     firstderiv = snow_property.diff() / snow_property.index.to_series().diff().dt.total_seconds()
     
@@ -114,7 +192,17 @@ def calc_sdd(snow_property, verbose=False, snow_name=None):
     return snow_all_gone_date, firstderiv
 
 def calc_peak(snow_property, verbose=False, snow_name=None, units='m'):
-    '''Finds date of maximum snow value from a pandas series of snow depth or SWE
+    '''
+    Finds the date of the maximum snow value from a pandas series of snow depth or SWE.
+
+    Parameters:
+        snow_property (pandas.Series): A pandas series containing snow depth or SWE values.
+        verbose (bool, optional): If True, prints additional information. Defaults to False.
+        snow_name (str, optional): The name of the snow property. Defaults to None.
+        units (str, optional): The units of the snow property. Defaults to 'm'.
+
+    Returns:
+        tuple: A tuple containing the peak date and the maximum value of the snow property.
     '''
     # Determine date of maximum value in snow property
     peak_date = snow_property.idxmax()
@@ -129,3 +217,36 @@ def calc_peak(snow_property, verbose=False, snow_name=None, units='m'):
         print(f'Peak {snow_name} value: {max_val} {units}')
     
     return peak_date, max_val
+
+def locate_snotel_in_poly(poly_fn: str, site_locs_fn: str):
+    '''
+    Extract snotel sites located within a given polygon.
+
+    Args:
+        poly_fn (str): The filepath of the polygon.
+        site_locs_fn (str): The filepath of the snotel sites.
+
+    Returns:
+        geopandas.GeoDataFrame: A geodataframe of snotel sites located within the polygon.
+    
+    Notes:
+        site_locs_fn:   EPSG 4326 /uufs/chpc.utah.edu/common/home/skiles-group3/SNOTEL/snotel_sites.json
+                        EPSG 32613 /uufs/chpc.utah.edu/common/home/skiles-group3/SNOTEL/snotel_sites_32613.json
+        poly_fn: '/uufs/chpc.utah.edu/common/home/skiles-group1/jmhu/ancillary/polys/yampa.shp'
+    '''
+    sites_gdf = gpd.read_file(site_locs_fn)
+    poly_gdf = gpd.read_file(poly_fn)
+
+    # Merge geometries if multipart polygon
+    if len(poly_gdf.geometry) > 1:
+        poly_geom = unary_union(poly_gdf.geometry)
+    else:        
+        poly_geom = poly_gdf.iloc[0].geometry
+    
+    # Check if sites are located within polygon
+    idx = sites_gdf.intersects(poly_geom)
+    
+    # Extract sites located within polygon
+    poly_sites = sites_gdf.loc[idx]
+    
+    return poly_sites
